537e94f3a7eb196d5c6b6f72f25e00f83207f8e1
nknodev <52920235+nknodev@users.noreply.github.com>
2022-04-17 14:07:23 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Shirix'
+copyright = '2022, nknodev'
+author = 'nknodev'
 
 release = '0.1'
 version = '0.1.0'
